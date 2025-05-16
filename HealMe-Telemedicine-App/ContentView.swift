e2002b36bd71ba74ffc1d19b39fa4ca6cd715de1
Cristian <mmcris2003@hotmail.com>
2025-05-16 18:47:07 -0500

    Initial Commit

---
+++ HealMe-Telemedicine-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealMe-Telemedicine-App
+//
+//  Created by Cristian Montiel García on 16/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealMe-Telemedicine-App/HealMe_Telemedicine_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealMe_Telemedicine_AppApp.swift
+//  HealMe-Telemedicine-App
+//
+//  Created by Cristian Montiel García on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HealMe_Telemedicine_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
